63d4837fddd99d8711e7241ebd7099411ebfa5f5
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-08-23 10:29:22 -0500

    12+~(1) starting with angular projects
    + videos from 1 to 6
    + ng serve -o

---
+++ AngularBasics/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+/* Angular Basics
+https://www.youtube.com/watch?v=NMzl2pGOK_8&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=1&pp=iAQB - Introduction to Angular
+https://www.youtube.com/watch?v=aSN-PIiaNN0&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=2&pp=iAQB - Angular Project Setup & Angular CLI
+https://www.youtube.com/watch?v=Frm2nBrdypg&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=3&pp=iAQB - Working with Angular Project
+https://www.youtube.com/watch?v=CV9BfQiPWKA&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=4&pp=iAQB - Angular files & Folder structure
+https://www.youtube.com/watch?v=RDytR8arAJk&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=5&pp=iAQB - What is TypeScript
+https://www.youtube.com/watch?v=ua2NONiy-7A&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=6&pp=iAQB - How Angular project is Executed */
