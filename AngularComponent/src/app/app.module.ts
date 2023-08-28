14b59ba2581cbde7cbe82ac9481b994708cc88a5
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-08-28 16:10:46 -0500

    12+~(4) one way data binding 2
    - video 16
    - event data binding (view to class)

--- AngularComponent/src/app/app.module.ts
+++ AngularComponent/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
-import { NotificationComponent } from './notification/notification.component'; // here it's declared
+import { NotificationComponent } from './notification/notification.component'; 
+import { SearchComponent } from './search/search.component'; // here it's declared
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { NotificationComponent } from './notification/notification.component'; /
     ContainerComponent,
     NavComponent,
     HeaderComponent,
-    NotificationComponent // also here
+    NotificationComponent,
+    SearchComponent // also here
   ],
   imports: [
     BrowserModule
@@ -24,6 +26,4 @@ import { NotificationComponent } from './notification/notification.component'; /
 export class AppModule { }
 
 /* Data Binding
-https://www.youtube.com/watch?v=r8U2DVRArdY&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=13&pp=iAQB - What is data binding
-https://www.youtube.com/watch?v=mT5q6zFzs3k&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=14&pp=iAQB - What is String Interpolation
-https://www.youtube.com/watch?v=k0sbLR5X2oY&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=15&pp=iAQB - What is Property Binding */
+https://www.youtube.com/watch?v=qIgRaYGso34&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=16&pp=iAQB - What is Event Binding */
