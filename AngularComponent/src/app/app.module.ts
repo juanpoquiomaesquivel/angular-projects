c671269a2983c0f27dea02cd2fe6cf24a21097b1
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-09-04 10:27:21 -0500

    12+~(6) ngFor directive
    - videos from 18 to 19
    - using *ngFor estructural directive

--- AngularComponent/src/app/app.module.ts
+++ AngularComponent/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { NotificationComponent } from './notification/notification.component'; 
 import { SearchComponent } from './search/search.component'; // here it's declared
 import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FormsModule } from '@angular/forms';
     NavComponent,
     HeaderComponent,
     NotificationComponent,
-    SearchComponent // also here
+    SearchComponent,
+    ProductsComponent // also here
   ],
   imports: [
     BrowserModule,
@@ -27,5 +29,6 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppModule { }
 
-/* Data Binding
-https://www.youtube.com/watch?v=qDpn2QCPNaA&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=17&pp=iAQB - Two way data binding */
+/* Directives
+https://www.youtube.com/watch?v=Xc58hRwsvR8&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=18&pp=iAQB - What is Directives
+https://www.youtube.com/watch?v=U-rEXMveeaA&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=19&pp=iAQB - ngFor Directive in Angular */
