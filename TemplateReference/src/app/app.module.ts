17900c1dc7ded08afdaa58c0de84d9047713ae74
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-09-11 04:53:17 -0500

    12+~(13) template reference variable
    - video 27
    - working with the DOM and variables in directives

--- TemplateReference/src/app/app.module.ts
+++ TemplateReference/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule
@@ -14,3 +18,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/* Data Binding
+https://www.youtube.com/watch?v=af_Lw_gEHRw&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=27&pp=iAQB - Template Reference Variable */
