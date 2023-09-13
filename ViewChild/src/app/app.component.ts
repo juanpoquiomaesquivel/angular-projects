da8bad2d7bc81790e9a7f40605cacbc4043132d0
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-09-13 07:30:30 -0500

    12+~(14) using @ViewChild to access directives
    - video 28
    - includes child components

--- ViewChild/src/app/app.component.ts
+++ ViewChild/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ViewChild';
+
+  @ViewChild('dobInput') dateOfBirth: ElementRef;
+  @ViewChild('ageInput') age: ElementRef;
+  @ViewChild(DemoComponent, {static: true}) demoComp: DemoComponent;
+
+  calculateAge(){
+    let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+    // console.log(this.dateOfBirth);
+    // console.log(this.age);
+  }
 }
--- ViewChild/src/app/app.module.ts
+++ ViewChild/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule
@@ -14,3 +16,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/* Data Bingding 
+https://www.youtube.com/watch?v=tPYGJM1-qkk&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=28&pp=iAQB - @ViewChild in Angular */
