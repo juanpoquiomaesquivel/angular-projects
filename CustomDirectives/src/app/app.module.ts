05c5c80ac789eb01701ed47757b2950507b43150
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-09-20 21:21:12 -0500

    12+~(19) using custom attribute directives
    - video 34
    - creating attributes directives

--- CustomDirectives/src/app/app.module.ts
+++ CustomDirectives/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SetBackgroundDirective } from './CustomDirective/setbackground.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetBackgroundDirective
   ],
   imports: [
     BrowserModule
@@ -14,3 +16,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/* Directives
+https://www.youtube.com/watch?v=DxfAEhdOl_0&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=34&pp=iAQB - Custom Attribute Directive */
