693d20eff9ada36719206c848292ae65bd9244bf
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-09-22 05:05:48 -0500

    12+~(24) custom appClass directive = ngClass
    - video 39
    - it's similiar to ngClass

--- CustomDirectives/src/app/app.module.ts
+++ CustomDirectives/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SetBackgroundDirective } from './CustomDirective/setbackground.directiv
 import { HighlightDirective } from './CustomDirective/highlight.directive';
 import { HoverDirective } from './CustomDirective/hover.directive';
 import { BetterhighlightDirective } from './CustomDirective/betterhighlight.directive';
+import { ClassDirective } from './CustomDirective/class.directive';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { BetterhighlightDirective } from './CustomDirective/betterhighlight.dire
     SetBackgroundDirective,
     HighlightDirective,
     HoverDirective,
-    BetterhighlightDirective
+    BetterhighlightDirective,
+    ClassDirective
   ],
   imports: [
     BrowserModule
@@ -23,4 +25,4 @@ import { BetterhighlightDirective } from './CustomDirective/betterhighlight.dire
 })
 export class AppModule { }
 /* Directives
-https://www.youtube.com/watch?v=mnHhdlMefug&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=38&pp=iAQB - Binding Directive Properties */
+https://www.youtube.com/watch?v=_taBk-4Ddr0&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=39&pp=iAQB - Custom appClass directive */
