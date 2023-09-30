d90b26734933e732e31efb82aedcf29c94c7a7cc
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-09-30 02:07:41 -0500

    12+~(31) component interaction through services
    - video 51
    - communicating sibling components

--- UserDetailService/src/app/app.module.ts
+++ UserDetailService/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllUsersComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
@@ -14,3 +18,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/* Services & Dependency Injection
+https://www.youtube.com/watch?v=dcoiUJrdsP0&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=51&pp=iAQB - Component Interaction with Services */
