4395617db08d96c038fb37eb2c853327b024df0a
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-10-02 09:45:40 -0500

    12+~(32) observables: next, error, complete
    - video from 52 to 53

--- AngularObservables/src/app/app.component.ts
+++ AngularObservables/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularObservables';
+
+  // USING OBSERVABLE CONSTRUCTOR
+  myObservable = new Observable((observer) => {
+    console.log('Observable starts')
+    setTimeout(() => { observer.next("1") }, 1000)
+    setTimeout(() => { observer.next("2") }, 2000)
+    setTimeout(() => { observer.next("3") }, 3000)
+    // setTimeout(() => { observer.error(new Error('Something went wrong! Please try again later')) }, 3000)
+    setTimeout(() => { observer.next("4") }, 4000)
+    setTimeout(() => { observer.next("5") }, 5000)
+    setTimeout(() => { observer.complete() }, 3000)
+    // observer.next("1")
+    // observer.next("2")
+    // observer.next("3")
+    // observer.next("4")
+    // observer.next("5")
+  });
+
+  ngOnInit() {
+    this.myObservable.subscribe((val) => {
+      console.log(val);
+    }, (error) => {
+      alert(error.message);
+    }, () => {
+      alert('Observable has complete emitting all values.')
+    });
+  }
 }
--- AngularObservables/src/app/app.module.ts
+++ AngularObservables/src/app/app.module.ts
@@ -14,3 +14,6 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/* Observables
+https://www.youtube.com/watch?v=V4iMyVnQPqM&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=52&pp=iAQB - What is Observable
+https://www.youtube.com/watch?v=DgsMSLHM-fY&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=53&pp=iAQB - Error & Completion of Observable */
