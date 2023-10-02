4d0f5f88566e218119d7847003ce4d28c76f1200
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-10-02 09:59:19 -0500

    12+~(33) ways to create an observable
    - video 54

--- AngularObservables/src/app/app.component.ts
+++ AngularObservables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, filter, from, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +10,39 @@ export class AppComponent {
   title = 'AngularObservables';
 
   // USING OBSERVABLE CONSTRUCTOR
-  myObservable = new Observable((observer) => {
-    console.log('Observable starts')
-    setTimeout(() => { observer.next("1") }, 1000)
-    setTimeout(() => { observer.next("2") }, 2000)
-    setTimeout(() => { observer.next("3") }, 3000)
-    // setTimeout(() => { observer.error(new Error('Something went wrong! Please try again later')) }, 3000)
-    setTimeout(() => { observer.next("4") }, 4000)
-    setTimeout(() => { observer.next("5") }, 5000)
-    setTimeout(() => { observer.complete() }, 3000)
-    // observer.next("1")
-    // observer.next("2")
-    // observer.next("3")
-    // observer.next("4")
-    // observer.next("5")
-  });
+  // myObservable = new Observable((observer) => {
+  //   console.log('Observable starts')
+  //   setTimeout(() => { observer.next("1") }, 1000)
+  //   setTimeout(() => { observer.next("2") }, 2000)
+  //   setTimeout(() => { observer.next("3") }, 3000)
+  //   // setTimeout(() => { observer.error(new Error('Something went wrong! Please try again later')) }, 3000)
+  //   setTimeout(() => { observer.next("4") }, 4000)
+  //   setTimeout(() => { observer.next("5") }, 5000)
+  //   setTimeout(() => { observer.complete() }, 3000)
+  //   // observer.next("1")
+  //   // observer.next("2")
+  //   // observer.next("3")
+  //   // observer.next("4")
+  //   // observer.next("5")
+  // });
+
+  // USING CREATE METHOD - deprecated
+  // myObservable = Observable.create((observer) => {
+  //   setTimeout(() => { observer.next("A") }, 1000)
+  //   setTimeout(() => { observer.next("B") }, 2000)
+  //   setTimeout(() => { observer.next("C") }, 3000)
+  //   setTimeout(() => { observer.next("D") }, 4000)
+  //   // setTimeout(()=>{observer.error(new Error('Something went wrong! Please try again later'))}, 4000)
+  //   setTimeout(() => { observer.next("E") }, 5000)
+  //   setTimeout(() => { observer.complete() }, 7000)
+  // });
+
+  array1 = [1, 2, 6, 7, 8];
+  array2 = ['A', 'B', 'C'];
+
+  // myObservable = of(this.array1, this.array2, 20, 79, 'Hello');
+
+  myObservable = from(this.array1);
 
   ngOnInit() {
     this.myObservable.subscribe((val) => {
--- AngularObservables/src/app/app.module.ts
+++ AngularObservables/src/app/app.module.ts
@@ -15,5 +15,4 @@ import { AppComponent } from './app.component';
 })
 export class AppModule { }
 /* Observables
-https://www.youtube.com/watch?v=V4iMyVnQPqM&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=52&pp=iAQB - What is Observable
-https://www.youtube.com/watch?v=DgsMSLHM-fY&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=53&pp=iAQB - Error & Completion of Observable */
+https://www.youtube.com/watch?v=btZnsw2mLs0&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=54&pp=iAQB - Different ways of Creating Observable */
