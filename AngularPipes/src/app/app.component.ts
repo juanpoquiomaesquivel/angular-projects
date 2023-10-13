f92a5cf8f82b84ca22de18cfacbdb812ddf550eb
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-10-13 14:26:02 -0500

    13+~(48) angular pipes
    - videos from 76 to 82
    - custom pipes
    - filter pipe (not recommedable)
    - pure and impure pipes
    - data filtering & async pipe

--- AngularPipes/src/app/app.component.ts
+++ AngularPipes/src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from './student';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StudentService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularPipes';
+  students: Student[];
+  totalMarks: number;
+  _filterText: string = '';
+  filteredStudents: Student[];
+
+  totalStudents = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(this.filteredStudents.length);
+    }, 2000);
+  });
+
+  get filterText() {
+    return this._filterText;
+  }
+
+  set filterText(value: string) {
+    this._filterText = value;
+    this.filteredStudents = this.filterStudentByGender(value);
+  }
+
+  constructor(private studentService: StudentService) {
+
+  }
+
+  ngOnInit() {
+    this.students = this.studentService.students;
+    this.totalMarks = this.studentService.totalMarks;
+    this.filteredStudents = this.students;
+  }
+
+  AddDummyStudent() {
+    // let studentCopy = Object.assign([], this.students)
+    // studentCopy.push({name: 'TEST', course: 'TEST', marks: 520, DOB: new Date(), gender: 'Female'});
+    // this.students = studentCopy;
+    this.students.push({ name: 'TEST', course: 'TEST', marks: 520, DOB: new Date(), gender: 'Female' });
+    this.filteredStudents = this.filterStudentByGender(this._filterText);
+  }
+
+  ChangeGender() {
+    // let studentCopy = Object.assign([], this.students)
+    // studentCopy[0].gender = 'Female';
+    // this.students = studentCopy;
+    this.students[0].gender = 'Female';
+    this.filteredStudents = this.filterStudentByGender(this._filterText);
+  }
+
+  onMouseMove() { }
+
+  filterStudentByGender(filterTerm: string) {
+    if (this.students.length === 0 || filterTerm === '') {
+      return this.students;
+    } else {
+      return this.students.filter((student) => {
+        return student.gender.toLowerCase() === filterTerm.toLowerCase();
+      })
+    }
+  }
 }
--- AngularPipes/src/app/app.module.ts
+++ AngularPipes/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+import { PercentagePipe } from './percentage.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PercentagePipe,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* Angular Pipes
+https://www.youtube.com/watch?v=DOUmCfaYA1w&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=76&pp=iAQB - Pipes in Angular
+https://www.youtube.com/watch?v=cJK42g95d6w&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=77&pp=iAQB - Creating Custom Pipes in Angular
+https://www.youtube.com/watch?v=0IKsWNhBKNM&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=78&pp=iAQB - Creating Filter Pipe in Angular
+https://www.youtube.com/watch?v=ik7iPoPZzWA&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=79&pp=iAQB - Pure Pipes in Angular
+https://www.youtube.com/watch?v=z4isL0m-dWk&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=80&pp=iAQB - Impure Pipes in Angular
+https://www.youtube.com/watch?v=lTOQ7Fjhcvk&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=81&pp=iAQB - Data Filtering in Angular Component
+https://www.youtube.com/watch?v=ChMEpGhvuJA&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=82&pp=iAQB - async pipe in Angular */
