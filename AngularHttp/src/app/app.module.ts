65c9dbb0ca4fae9ad27622411d8da6e9f838f2ff
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-10-30 06:10:12 -0500

    13+~(51) angular http
    - from video 97 to 105
    - get, put, post, delete request
    - headers and params
    - handling errors

--- AngularHttp/src/app/app.module.ts
+++ AngularHttp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,22 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* Angular Http
+https://www.youtube.com/watch?v=M3vN0hrKgGQ&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=97&pp=iAQB - Introduction to HTTP requests in Angular
+https://www.youtube.com/watch?v=NFTNBgpxzw4&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=98&pp=iAQB - Setting up Firebase
+https://www.youtube.com/watch?v=oTObLWih_EA&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=99&pp=iAQB - Sending a Post Request
+https://www.youtube.com/watch?v=Nuh6hTDh31s&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=100&pp=iAQB - Fetching Data with Get requets
+https://www.youtube.com/watch?v=w8mw7xlhi9Y&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=101&pp=iAQB - Deleting Data with HTTP Delete Request
+https://www.youtube.com/watch?v=9ZFXXMFSs8A&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=102&pp=iAQB - Using Service for HTTP Request 
+https://www.youtube.com/watch?v=ASFZsGJ5ZuE&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=103&pp=iAQB - Update data with HTTP Put Request
+https://www.youtube.com/watch?v=tk4msPsp3kM&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=104&pp=iAQB - Handling HTTP Response Errors
+https://www.youtube.com/watch?v=mFxZ_ne3g3c&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=105&pp=iAQB - Setting Headers & Query Params in HTTP Requests */
--- ReactiveForm/src/app/app.module.ts
+++ ReactiveForm/src/app/app.module.ts
@@ -14,3 +14,13 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* Reactive Form
+https://www.youtube.com/watch?v=qOZOeu6YcJc&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=89&pp=iAQB - Creating and Using Reactive Forms
+https://www.youtube.com/watch?v=jdsSIKgJ6ZM&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=90&pp=iAQB - Form Validation of Reactive Forms
+https://www.youtube.com/watch?v=nc8FBBShlYo&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=91&pp=iAQB - Grouping of Form Controls
+https://www.youtube.com/watch?v=_By0TTKuxWI&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=92&pp=iAQB - What is Form Array
+https://www.youtube.com/watch?v=VmdSmeDpW0k&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=93&pp=iAQB - Custom Validator and Error Code
+https://www.youtube.com/watch?v=xbk2-z8gY4M&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=94&pp=iAQB - Custom async validator
+https://www.youtube.com/watch?v=vhifD4o1nUE&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=95&pp=iAQB - Value & Statuc Change events
+https://www.youtube.com/watch?v=Hw8OV4kcKgM&list=PL1BztTYDF-QNrtkvjkT6Wjc8es7QB4Gty&index=96&pp=iAQB - setValue() & patchValue() methods */
